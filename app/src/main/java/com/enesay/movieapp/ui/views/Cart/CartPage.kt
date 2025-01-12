c1d62d3948711252244a9d06b81f095f056ddfcc
Enes Ay <enesay233@gmail.com>
2025-01-12 11:14:20 +0300

    refactor(cart ui): Cart item refactored

--- app/src/main/java/com/enesay/movieapp/ui/views/Cart/CartPage.kt
+++ app/src/main/java/com/enesay/movieapp/ui/views/Cart/CartPage.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -48,7 +49,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
@@ -63,6 +66,8 @@ import androidx.navigation.NavHostController
 import com.enesay.movieapp.R
 import com.enesay.movieapp.data.model.Movie
 import com.enesay.movieapp.data.model.MovieCart
+import com.enesay.movieapp.ui.theme.PrimaryBlack
+import com.enesay.movieapp.ui.theme.PrimaryWhite
 import com.enesay.movieapp.utils.Constants.IMAGE_BASE_URL
 import com.google.gson.Gson
 import com.skydoves.landscapist.glide.GlideImage
@@ -79,14 +84,15 @@ fun CartPage(navController: NavHostController) {
     val cartLoadingState = cartViewmodel.moviesState.value
     //val cartItems by cartViewmodel.cartItems.observeAsState(initial = listOf())
 
-    val groupedMovies  by  remember {derivedStateOf {
-        cartItems.groupBy { it.name }.mapValues { entry ->
-            mapOf(
-                "amount" to entry.value.sumOf { it.orderAmount },
-                "ids" to entry.value.map { it.cartId }
-            )
+    val groupedMovies by remember {
+        derivedStateOf {
+            cartItems.groupBy { it.name }.mapValues { entry ->
+                mapOf(
+                    "amount" to entry.value.sumOf { it.orderAmount },
+                    "ids" to entry.value.map { it.cartId }
+                )
+            }
         }
-    }
     }
 
     val totalPrice = cartItems.sumOf { it.price * it.orderAmount }
@@ -113,14 +119,16 @@ fun CartPage(navController: NavHostController) {
         }
     ) { paddingValues ->
 
-        if(cartLoadingState is CartUiState.Loading){
-            Box(modifier = Modifier.fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
-                contentAlignment = Alignment.Center){
+        if (cartLoadingState is CartUiState.Loading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background),
+                contentAlignment = Alignment.Center
+            ) {
                 CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
             }
-        }
-        else if (cartItems.isEmpty()) {
+        } else if (cartItems.isEmpty()) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -134,8 +142,7 @@ fun CartPage(navController: NavHostController) {
                     fontSize = 18.sp
                 )
             }
-        }
-        else {
+        } else {
 
             Column(
                 modifier = Modifier
@@ -148,7 +155,9 @@ fun CartPage(navController: NavHostController) {
                         .weight(3f)
                         .padding(paddingValues)
                 ) {
-                    items(groupedMovies.keys.toList(), key = { movieName -> movieName.hashCode() }) { movieName ->
+                    items(
+                        groupedMovies.keys.toList(),
+                        key = { movieName -> movieName.hashCode() }) { movieName ->
                         val movieInfo = groupedMovies[movieName]
                         val orderAmount = movieInfo?.get("amount") as? Int ?: 0
                         val cartIds = movieInfo?.get("ids") as? List<Int> ?: emptyList()
@@ -237,128 +246,148 @@ fun CartItem(
     onAddToCart: () -> Unit,
 ) {
     var count by remember { mutableIntStateOf(movie.orderAmount) }
-    Log.d("cart item", "count: $count")
+    val gradientColors = listOf(
+        Color(0xFF0D0D0D), // Dark Black
+        Color(0xFF262626)  // Light Black
+    )
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(12.dp) // Add padding around the card
-            .shadow(8.dp, RoundedCornerShape(16.dp)), // Add shadow and round corners
-        shape = RoundedCornerShape(16.dp), // Rounded corners
-        // elevation = 8.dp, // Elevation for the card shadow
+            .padding(12.dp)
+            .shadow(8.dp, RoundedCornerShape(16.dp)),
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Row(
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.primary)
-                .height(120.dp)
+                .background(Brush.horizontalGradient(gradientColors))
                 .clickable { onClick() }
-                .padding(vertical = 4.dp, horizontal = 10.dp)
         ) {
-            Column(
+            Row(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1.5f)
+                    .fillMaxWidth()
+                    .height(140.dp)
+                    .padding(12.dp)
             ) {
-
+                // Movie Thumbnail
                 GlideImage(
                     modifier = Modifier
-                        .size(170.dp)
-                        .weight(1f),
+                        .size(100.dp, 120.dp)
+                        .clip(RoundedCornerShape(8.dp)),
                     imageModel = "$IMAGE_BASE_URL${movie.image}",
-                    contentScale = ContentScale.Crop,
+                    contentScale = ContentScale.Crop
                 )
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1.5f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
+                Spacer(modifier = Modifier.width(22.dp))
 
-                Text(
-                    text = movie.name,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 22.sp,
-                    textAlign = TextAlign.Center
-                )
-
-                Text(
-                    text = "${movie.price * movie.orderAmount}₺",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(2f),
-                horizontalAlignment = Alignment.End
-            ) {
-
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    verticalAlignment = Alignment.CenterVertically
+                // Movie Details
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 8.dp)
+                    .weight(1.3f),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = movie.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Text(
+                        text = movie.category,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White.copy(alpha = 0.8f)
+                    )
+                    Text(
+                        text = "IMDb: ${movie.rating}",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.Yellow.copy(alpha = 0.9f)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "${movie.price * count}₺",
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                // Quantity Controls
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1.5f),
+                    horizontalAlignment = Alignment.End
                 ) {
 
                     Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.SpaceAround,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        if (count == 1) {
-                            // Movie remove button
-                            IconButton(
-                                modifier = Modifier.size(36.dp), // Set fixed size for the button
-                                onClick = {
-                                    count--
-                                    onDelete()
-                                }
-                            ) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_delete),
-                                    contentDescription = "icon delete"
-                                )
-                            }
-                        } else if (count > 0) {
-                            // Movie remove button
-                            IconButton(
-                                modifier = Modifier.size(36.dp), // Set fixed size for the button
-                                onClick = {
-                                    count--
-                                    onDelete()
-                                }
-                            ) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_remove_circle),
-                                    contentDescription = "icon remove"
-                                )
-                            }
-                        } else {
-                            Spacer(modifier = Modifier.size(36.dp)) // Reserve space when "-" button is not visible
-                        }
 
-                        if (count > 0) {
-                            Text(
-                                text = "$count",
-                                fontSize = 22.sp,
-                                modifier = Modifier.padding(horizontal = 16.dp)
-                            )
-                        }
-
-                        IconButton(
-                            modifier = Modifier.size(36.dp),
-                            onClick = {
-                                count++
-                                onAddToCart()
-                            }
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Icon(Icons.Default.AddCircle, contentDescription = "Increase Button")
+                            if (count == 1) {
+                                // Movie remove button
+                                IconButton(
+                                    modifier = Modifier.size(36.dp), // Set fixed size for the button
+                                    onClick = {
+                                        count--
+                                        onDelete()
+                                    }
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_delete),
+                                        contentDescription = "icon delete",
+                                        tint = PrimaryWhite
+                                    )
+                                }
+                            } else if (count > 0) {
+                                // Movie remove button
+                                IconButton(
+                                    modifier = Modifier.size(36.dp), // Set fixed size for the button
+                                    onClick = {
+                                        count--
+                                        onDelete()
+                                    }
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_remove_circle),
+                                        contentDescription = "icon remove",
+                                        tint = PrimaryWhite
+                                    )
+                                }
+                            } else {
+                                Spacer(modifier = Modifier.size(36.dp)) // Reserve space when "-" button is not visible
+                            }
+
+                            if (count > 0) {
+                                Text(
+                                    text = "$count",
+                                    fontSize = 22.sp,
+                                    modifier = Modifier.padding(horizontal = 16.dp),
+                                    color = PrimaryWhite
+                                )
+                            }
+
+                            IconButton(
+                                modifier = Modifier.size(36.dp),
+                                onClick = {
+                                    count++
+                                    onAddToCart()
+                                }
+                            ) {
+                                Icon(
+                                    Icons.Default.AddCircle,
+                                    contentDescription = "Increase Button",
+                                    tint = PrimaryWhite
+                                )
+                            }
                         }
                     }
                 }
             }
         }
     }
-
-
 }
